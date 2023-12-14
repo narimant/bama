92017bd5aba7d4d997093ced1708cac25591f7ba
nariman <nariman.tatari@gmail.com>
2023-12-15 02:03:45 +0330

    fix menu for hover and open child menu and home filter

--- components/template/HomeHeader.js
+++ components/template/HomeHeader.js
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import background from "@public/images/background.jpg"
 import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import Link from "next/link";
+import { ImFilter } from "react-icons/im";
 
 const HomeHeader = () => {
     const [blur,setBlur]=useState(false);
@@ -19,15 +22,23 @@ const HomeHeader = () => {
         <div className="flex justify-center items-center h-[calc(100svh-128px)] overflow-hidden">
         <Image src={background} alt="Some text" />
         </div>
-        <div onClick={blurActive} className="w-3/5 h-28 bg-white text-black z-10  absolute -bottom-10 border-gray-200 border flex justify-between">
-        <div>
-
+        <div  className="w-3/5 h-28 bg-white text-black z-10  absolute -bottom-10 border-gray-200 border grid grid-cols-3">
+        <div className="flex justify-center items-start w-full flex-col pr-6">
+            <p className="flex justify-between w-60 items-center text-base font-bold cursor-pointer" onClick={blurActive}>
+                <span>برند و مدل</span>
+                <IoIosArrowDown />
+            </p>
+            <p className="text-xs">تمام برندها و مدل ها</p>
         </div>
-            <div>
-
+            <div className="flex justify-center items-center">
+            <div class="slider"><input type="range" min="0" max="200" value="100" oninput="rangeValue.innerText = this.value" className="w-full" /><p id="rangeValue">100</p></div> 
+   
             </div>
-            <div>
-
+            <div className="flex justify-center items-center">
+                <Link href=""  className="bg-lime-400 flex items-center gap-2 py-3 px-6 rounded-3xl font-bold">
+                <ImFilter />
+                   <span> نمایش آگهی ها (2 فیلتر)</span>
+                    </Link>
             </div>
         </div>
         </div>
--- components/template/Navbar.js
+++ components/template/Navbar.js
@@ -1,76 +1,95 @@
 import Link from "next/link";
+import { FaCarRear } from "react-icons/fa6";
+import { IoMdAdd } from "react-icons/io";
+import { GrLineChart } from "react-icons/gr";
+import { PiEngine } from "react-icons/pi";
+import { BsNewspaper } from "react-icons/bs";
+
 
 const Navbar = () => {
   return (
-    <div className="w-full  bg-[#FBFBFB]  shadow-md py-2 text-center font-Vazirmatn text-base font-medium">
-      <div className="container mx-auto flex justify-between items-center">
+    <div className="w-full  bg-[#FBFBFB]  shadow-md text-center font-Vazirmatn text-base font-medium">
+      <div className="container mx-auto flex justify-between  items-center">
         <div>
-          <ul className="flex">
-            <li className="relative">
-              <Link href="/car" className="py-4 pl-8">
+          <ul className="flex justify-between items-center" >
+            <li  className="group ">
+              <Link href="/car" className="flex pl-8 py-5 ">
                 خودرو
               </Link>
-              <div className="absolute z-20 bg-gray-200 mt-7 w-52">
-                <div className="flex flex-col  items-start">
-                <Link href='' className="py-3 px-4">
 
-                <span>خرید خودرو</span>
-                </Link>
-                <Link href='' className="py-4 px-4 flex  items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
+              {/* ----child menu---- */}
+              <div className="absolute hidden flex-col z-20 bg-white  w-60 items-center group-hover:flex">
+                <div className="flex flex-col p-2 w-full bg-gray-200">
 
-                <span>ثبت آگهی خودرو</span>
-                </Link>
-                <Link href=''>
-                <span>قیمت روز خودرو</span>
-                </Link>
-                <Link href=''>
-                <span>مشخصات فنی خودرو</span>
-                </Link>
-                <Link href=''>
-                <span>اخبار خودرو</span>
-                </Link>
+                  <Link
+                    href=""
+                    className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full"
+                  >
+                    <FaCarRear className="w-6 h-6" />
+                    <span>خرید خودرو</span>
+                  </Link>
 
+                  <Link href="" className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full">
+                  <IoMdAdd  className="w-6 h-6"/>
+                    <span>ثبت آگهی خودرو</span>
+                  </Link>
+
+                  <Link href="" className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full">
+                    <GrLineChart  className="w-6 h-6"/>
+                    <span>قیمت روز خودرو</span>
+                  </Link>
+                  <Link href="" className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full">
+                    <PiEngine  className="w-6 h-6"/>
+                    <span> مشخصات فنی خودرو </span>
+                  </Link>
+
+
+                  <Link href="" className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full">
+                    <BsNewspaper  className="w-6 h-6"/>
+                    <span>اخبار خودرو</span>
+                  </Link>
                 </div>
-                <div>
-                <Link href=''><span>استغلام خلافی خودرو</span></Link>
-                <Link href=''><span>شرایط فروش خودروسازها</span></Link>
-                <Link href=''><span>ثبت نام نمایشگاه خودرو</span></Link>          
+
+                <div className="bg-gray-200 mt-1 w-full font-light text-sm">
+                  <Link href="" className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full">
+                    <span>استغلام خلافی خودرو</span>
+                    <span className="bg-red-600 text-white text-[10px] px-1 py-0 rounded-md">جدید</span>
+                  </Link>
+                  <Link href="" className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full">
+                    <span>شرایط فروش خودروسازها</span>
+                  </Link>
+                  <Link href="" className=" flex  py-3 px-4 gap-2 hover:bg-gray-50 w-full">
+                    <span>ثبت نام نمایشگاه خودرو</span>
+                  </Link>
                 </div>
-               
               </div>
             </li>
             <li>
-              <Link href="/car" className="py-4 px-4">
+              <Link href="/car" className="py-5 px-4">
                 موتور سیکلت
               </Link>
             </li>
             <li>
-              <Link href="/car" className="py-4 px-4">
+              <Link href="/car" className="py-5 px-4">
                 سنگین
               </Link>
             </li>
             <li>
-                
-              <Link href="/car" className="py-4 px-4">
+              <Link href="/car" className="py-5 px-4">
                 باما
               </Link>
             </li>
           </ul>
-        </div>
-
+          </div>
 
         <p>
           <Link href="/">
             <svg
-              width="81"
-              height="65"
+           
               viewBox="0 0 81 65"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="bama-logo__svg"
+              className="h-12"
               data-v-5561ece8=""
             >
               <path
@@ -102,8 +121,6 @@ const Navbar = () => {
           </Link>
         </p>
 
-
-
         <div>
           <ul className="flex justify-between items-center">
             <li>
@@ -120,8 +137,8 @@ const Navbar = () => {
                   className="w-6 h-6"
                 >
                   <path
-                   strokeLinecap="round"
-                   strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
                   />
                 </svg>
@@ -133,21 +150,21 @@ const Navbar = () => {
                 href="/create-advertise"
                 className="py-1 px-4 flex justify-between items-center gap-2 hover:bg-gray-200 rounded-full border border-gray-200"
               >
-               <span className="bg-lime-400 rounded-full p-2"> 
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                   strokeLinecap="round"
-                   strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
+                <span className="bg-lime-400 rounded-full p-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
                 </span>
                 ثبت آگهی
               </Link>
